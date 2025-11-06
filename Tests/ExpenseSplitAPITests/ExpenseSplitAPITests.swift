74e85ef72bdcb31dbddac0b91d85bc132c6b4ce8
arthur <arthur.rios007@gmail.com>
2025-11-06 20:57:52 -0300

    Generate Vapor project

---
+++ Tests/ExpenseSplitAPITests/ExpenseSplitAPITests.swift
@@ -0,0 +1,16 @@
+@testable import ExpenseSplitAPI
+import VaporTesting
+import Testing
+
+@Suite("App Tests")
+struct ExpenseSplitAPITests {
+    @Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp(configure: configure) { app in
+            try await app.testing().test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
+    }
+}
